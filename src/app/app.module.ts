ac9bea92665a1c8bf902dacbcfd0c1ad46efbbda
Yasith Ishara Perera <yasithishara@gmail.com>
2025-01-09 21:47:47 +0530

    created gov-donation service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
