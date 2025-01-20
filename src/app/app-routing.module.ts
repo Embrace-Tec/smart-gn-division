2923a6695a4ae73a3487bd30af0e1ae7bd344f7d
lahiruharshana <lharshana2002@gmail.com>
2025-01-20 21:30:13 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StepperFormComponent } from './components/stepper-form/stepper-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'stepper-form', pathMatch: 'full' }, // Redirect root to the stepper
+  { path: 'stepper-form', component: StepperFormComponent },  // Define route for StepperFormComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { StepperFormComponent } from './components/stepper-form/stepper-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StepperFormComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync(),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
